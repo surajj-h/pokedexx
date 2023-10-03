9ae3f746b704e62700737a48d68b103b05a3ace9
suraj h <surajj.hiremath@gmail.com>
2023-10-04 01:12:24 +0530

    Pokemons data added with search feature

--- src/App.js
+++ src/App.js
@@ -6,27 +6,33 @@ class App extends Component {
   constructor(){
     super();
     this.state={
-      pokemons:[
-        {
-        name:'pickachu'
-      },
-      {
-        name:'charizard'
-      }
-      ,{
-        name:'bulbasor'
-      }
-    ]
-
+      pokemons:[],
+      search_pokemons:[]
     }
   }
+
+  componentDidMount(){
+    fetch('https://api.jsonbin.io/v3/b/651470d754105e766fba96ac')
+    .then((response) => response.json())
+    .then((records) => this.setState(() => {
+      return {pokemons :records.record,search_pokemons :records.record}
+    }))
+    .catch(error => console.log("Invalid API request")
+    );
+  }
+
 
   render(){
     return (
       <div className="App">
+      <input className="search-box" type="search" placeholder="search pokemon" onChange={(event)=>{
+        this.setState(()=>{
+          return {search_pokemons : this.state.pokemons.filter((pokemon) => pokemon.name.english.toLowerCase().includes(event.target.value.toLowerCase()))}
+        })
+      }} />
       {
-        this.state.pokemons.map((pokemon) => {
-          return <h1>{pokemon.name}</h1>
+        this.state.search_pokemons.map((pokemon) => {
+          return <h1 key={pokemon.id}>{pokemon.name.english}</h1>
         })
       }
       </div>
